c93cdf92ba9b95799207b9bf4706f32a13880be3
joeco <joe@joeco.info>
2017-04-08 09:51:02 -0400

    needed chgs for proper npm publishing / npm linking w/out webpack errors

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require('./dist/index');
+var path = require('path');
+module.exports = {
+  context: path.join(__dirname, 'dist', 'index'),
+  resolve: { fallback: path.join(__dirname, "node_modules") },
+  resolveLoader: { fallback: path.join(__dirname, "node_modules") }
+};
